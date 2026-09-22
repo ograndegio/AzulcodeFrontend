4df4fe3baf461e2dd26ac8c8324193ef09e8f8d9
Giovanne Batista <giobatista14@gmail.com>
2026-09-21 21:14:39 -0400

    Atualização de Projetos

--- src/app/core/constants/projects.constants.ts
+++ src/app/core/constants/projects.constants.ts
@@ -2,10 +2,17 @@ import { Project } from '../models/project.model';
 
 export const PROJECTS: Project[] = [
     {
-        name: 'BlueWash',
+        name: 'Bluewash',
         description: 'Plataforma Corporativa para Gestão Financeira e Operacional de Lavanderias Self-service.',
-        url: 'https://bluewash.com.br',
+        url: 'https://www.bluewash.com.br',
         image: 'assets/images/projects/image.png',
-        stack: ['Angular', '.NET Core', 'SQL Server', 'Redis']
+        stack: ['Angular', '.NET Core', 'SQL Server', 'Redis', 'Kafka']
+    },
+    {
+        name: 'Azulstore',
+        description: 'Plataforma de E-commerce para venda de produtos online.',
+        url: 'https://www.azulstore.com.br',
+        image: 'assets/images/projects/image.png',
+        stack: ['Angular', '.NET Core', 'SQL Server', 'Redis', 'RabbitMQ']
     }
 ];
